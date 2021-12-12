a235c4faed4a9df04b0e1ac355a86396fc7440d0
Kiptoo Rugut <mccrug97@gmail.com>
2021-12-12 23:40:47 +0300

    It will add the formula of the dates.

--- js/script.js
+++ js/script.js
@@ -9,6 +9,8 @@ function getAkanName() {
     MM = getMonth();
     DD = getDay();
 
-
     var male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
     var female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+    d = (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7
+    d = Math.trunc(d);
