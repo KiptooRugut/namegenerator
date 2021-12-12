d05ad95dd3c88336e66d19278a51220fff412c3b
Kiptoo Rugut <mccrug97@gmail.com>
2021-12-12 23:38:42 +0300

    It will add an array of female Akan names.

--- js/script.js
+++ js/script.js
@@ -11,3 +11,4 @@ function getAkanName() {
 
 
     var male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    var female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
